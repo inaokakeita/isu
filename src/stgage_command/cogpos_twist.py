6e8dcd2cd58f7da28a805e74e6dfd09d364ede78
Inaoka Keita <inaokakeita@gmail.com>
2018-10-16 19:33:26 +0900

    start/stop and coginit add.

--- src/stgage_command/cogpos_twist.py
+++ src/stgage_command/cogpos_twist.py
@@ -3,7 +3,6 @@ import rospy
 import math
 from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Joy
 
 g_twist_pub = None
 g_target_twist = None 
@@ -12,7 +11,6 @@ g_last_send_time = None
 g_vel_scales = [0.1, 0.1, 0.1]
 g_vel_ramps = [1, 1, 1] # units: meters per second^2
 g_cogpos = Float32MultiArray()#Centor Of Gravity Position on seat [x,y]
-g_joy = Joy()
 
 def ramped_vel(v_prev, v_target, t_prev, t_now, ramp_rate):
   # compute maximum velocity step
@@ -30,21 +28,20 @@ def ramped_twist(prev, target, t_prev, t_now, ramps):
                             t_now, ramps[2])
     tw.linear.x = ramped_vel(prev.linear.x, target.linear.x, t_prev,
                            t_now, ramps[0])
-    tw.linear.y = ramped_vel(prev.linear.y, target.linear.y, t_prev,
-                           t_now, ramps[1])
+  #  tw.linear.y = ramped_vel(prev.linear.y, target.linear.y, t_prev,
+  #                         t_now, ramps[1])
     return tw
 #todo stop moving at buttons pushed
-def calc_vel(cogpos,joy,vel_scales):
+def calc_vel(cogpos,vel_scales):
     tw = Twist()
-    tw.linear.x=cogpos[0]*vel_scales[0]
-    tw.linear.y=cogpos[1]*vel_scales[1]
-    tw.angular.z = joy.axes[2]*vel_scales[2]
+    tw.linear.x=cogpos.data[0]*vel_scales[0]
+    tw.linear.y=cogpos.data[1]*vel_scales[1]
     return tw
 
 def send_twist():
     global g_last_twist_send_time, g_target_twist, g_last_twist,\
-            g_vel_ramps, g_twist_pub,g_cogpos,g_joy,g_vel_scales
-    g_last_twist=calc_vel(g_cogpos,g_joy,g_vel_scales)
+            g_vel_ramps, g_twist_pub,g_cogpos,g_vel_scales
+    g_last_twist=calc_vel(g_cogpos,g_vel_scales)
     t_now = rospy.Time.now()
     g_last_twist = ramped_twist(g_last_twist, g_target_twist,
                               g_last_twist_send_time, t_now, g_vel_ramps)
@@ -55,10 +52,6 @@ def cogpos_cb(msg):
     global g_cogpos
     g_cogpos = msg
     send_twist()
-
-def joy_cb(msg):
-  global g_joy
-  g_joy = msg
 
 def fetch_param(name, default):
   if rospy.has_param(name):
@@ -71,7 +64,6 @@ if __name__=="__main__":
     rospy.init_node('cogpos_twist')
     g_last_twist_send_time = rospy.Time.now()
     rospy.Subscriber('cogpos',Float32MultiArray,cogpos_cb)
-    rospy.Subscriber('joy'Joy,joy_cb)
     g_twist_pub = rospy.Publisher('cmd_vel',Twist,queue_size=1)
     g_vel_scales[0] = fetch_param('~linear_scale_x', 1)
     g_vel_scales[1] = fetch_param('~linear_scale_y', 1)
--- src/stgage_command/stgage_cogpos.py
+++ src/stgage_command/stgage_cogpos.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python
 import rospy
 import math
+from std_msgs.msg import Int32MultiArray
 from std_msgs.msg import Float32MultiArray
+
 from sensor_msgs.msg import Joy
 
 g_cogpos_pub = None
 g_stg = [0.01, 0.01, 0.01, 0.01] #stgage sensor raw data
-g_cog_offset = [0.0,0.0]
+g_cog_offset = Float32MultiArray()
+g_cog_offset.data = [0.0,0.0]
 g_stg_x = [0.0, 0.2, 0.2, 0.0]#straigngage sensor position 
 g_stg_y = [0.0, 0.0, 0.2, 0.2]
 g_rate = 20
 g_stg_init_flg = True
+g_joy = Joy()
+
 
 #todo stop moving at buttons pushed
 def calc_cog(stg,stg_x,stg_y):
-    cog[0] =(stg[0]*stg_x[0]+stg[1]*stg_x[1]+stg[2]*stg_x[2]+stg[3]*stg_x[3])/(stg[0]+stg[1]+stg[2]+stg[3])
-    cog[1]=(stg[0]*stg_y[0]+stg[1]*stg_y[1]+stg[2]*stg_y[2]+stg[3]*stg_y[3])/(stg[0]+stg[1]+stg[2]+stg[3])#calc Centor Of Gravity Position
+    cog = Float32MultiArray()
+    cog.data=[0.0,0.0]
+    if  (stg[0]+stg[1]+stg[2]+stg[3]) != 0:
+        cog.data[0] =(stg[0]*stg_x[0]+stg[1]*stg_x[1]+stg[2]*stg_x[2]+stg[3]*stg_x[3])/(stg[0]+stg[1]+stg[2]+stg[3])
+        cog.data[1]=(stg[0]*stg_y[0]+stg[1]*stg_y[1]+stg[2]*stg_y[2]+stg[3]*stg_y[3])/(stg[0]+stg[1]+stg[2]+stg[3])#calc Centor Of Gravity Position
     return cog
 
 def send_cogpos():
-    global g_stg,g_stg_x,g_stg_y,g_joy,g_cog_offset
-    cogpos=calc_vel(g_stg,g_stg_x,g_stg_y)
+    global g_stg,g_stg_x,g_stg_y,g_cog_offset,g_stg_init_flg
+    cogpos=calc_cog(g_stg,g_stg_x,g_stg_y)
     if g_stg_init_flg == True:
-        g_cog_offset = cogpos
-        cogpos = [0.0, 0.0]
+        g_cog_offset.data = cogpos.data
+        cogpos.data = [0.0, 0.0]
     else:
-        cogpos = cogpos - g_cog_offset
+        cogpos.data[0] = cogpos.data[0] - g_cog_offset.data[0]
+        cogpos.data[1] = cogpos.data[1] - g_cog_offset.data[1]
     g_cogpos_pub.publish(cogpos)
 
 def stgage_cb(msg):
@@ -35,10 +44,14 @@ def stgage_cb(msg):
 
 def joy_cb(msg):
     global g_stg_init_flg
-    if g_joy.buttons[6]==1 and g_joy.buttons[7]==1:
-        g_stg_init_flg = not(g_stg_init_flg)
-        rospy.sleep(0.5)
-      
+    if msg.buttons[4]==1 and msg.buttons[5]==1:
+        if g_stg_init_flg == False:
+            rospy.logwarn('stop')
+            g_stg_init_flg = True
+    if msg.buttons[6]==1 and msg.buttons[7]==1:
+        if g_stg_init_flg == True:
+            rospy.logwarn('start')
+            g_stg_init_flg = False
 
 def fetch_param(name, default):
   if rospy.has_param(name):
@@ -49,7 +62,7 @@ def fetch_param(name, default):
 
 if __name__=="__main__":
     rospy.init_node('stgage_cogpos')
-    rospy.Subscriber('stgage',Float32MultiArray,stgage_cb)
+    rospy.Subscriber('stg_array',Int32MultiArray,stgage_cb)
     rospy.Subscriber('joy',Joy,joy_cb)
     g_cogpos_pub = rospy.Publisher('cogpos',Float32MultiArray,queue_size=1)
     g_rate = fetch_param('~rate',20)
