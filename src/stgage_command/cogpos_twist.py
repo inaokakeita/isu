4a909176b6a217ab864c7de4471409e63e491798
inaokakeita <inaokakeita@inaoka-MacBook-Pro.local>
2018-11-27 22:46:45 +0900

    convert to polar coordinate

--- src/stgage_command/cogpos_twist.py
+++ src/stgage_command/cogpos_twist.py
@@ -10,9 +10,10 @@ g_last_twist = None
 g_last_send_time = None
 g_vel_scales = [0.1, 0.1, 0.1]
 g_vel_ramps = [1, 1, 1] # units: meters per second^2 [x,y,z]
-g_vel_max = [0.0,0.0,0.0] #units: meter per second [x,y,z] 
+g_vel_max = None #units: meter per second
+g_angular_max = None
 g_cogpos = Float32MultiArray()#Centor Of Gravity Position on seat [x,y]
-g_deadzone = [0.1,0.1]
+g_deadzone = None
 
 def ramped_vel(v_prev, v_target, t_prev, t_now, ramp_rate):
   # compute maximum velocity step
@@ -34,42 +35,41 @@ def ramped_twist(prev, target, t_prev, t_now, ramps, max):
                            t_now, ramps[1])
     return tw
 
-def calc_vel(cogpos,vel_scales,vel_max,deadzone):
+def calc_vel(cogpos,vel_scales,vel_max,angular_max,deadzone):
     tw = Twist()
     #mapping cogpos to velocity
     tw.linear.x=cogpos.data[0]*vel_scales[0]
     tw.linear.y=-cogpos.data[1]*vel_scales[1]
-
-    #vel_angle = math.atan2(tw.linear.y/tw.linear.x)
-    #vel_r = sqrt(pow(tw.linear.x,2)+pow(tw.linear.y,2))
+    #convert cartesian to polar coordinate
+    vel_angle = math.atan2(tw.linear.y,tw.linear.x)
+    vel_r = math.sqrt(math.pow(tw.linear.x,2)+math.pow(tw.linear.y,2))
     #set maxspeed
-    if tw.linear.x > vel_max[0]:
-       tw.linear.x = vel_max[0]
-    elif tw.linear.x < -vel_max[0]:
-      tw.linear.x = -vel_max[0]
-    if tw.linear.y > vel_max[1]:
-      tw.linear.y = vel_max[1]
-    elif tw.linear.y < -vel_max[1]:
-      tw.linear.y = -vel_max[1]
-    #mapping rotate
-    tw.angular.z
+    if vel_r > vel_max:
+      vel_r = vel_max
     #set dead one
-    if tw.linear.x < deadzone[0]:
-      tw.linear.x = 0.0
-    if tw.linear.y < deadzone[1] and tw.linear.y > 0.0:
-      tw.linear.y = 0.0
-    elif tw.linear.y > -deadzone[1] and tw.linear.y < 0.0:
-      tw.linear.y = 0.0
+    if vel_r < deadzone:
+        vel_r = 0.0
+    #mapping rotate
+    if vel_angle > math.pi/2:
+      tw.angular.z = -vel_r * vel_scales[2]
+      if tw.angular.z < -angular_max:
+        tw.angular.z = -angular_max
+    elif  vel_angle < -math.pi/2:
+      tw.angular.z = vel_r * vel_scales[2]
+      if tw.angular.z > angular_max:
+        tw.angular.z = angular_max
     #forbid back
-    if tw.linear.x < 0.0:
-      tw.linear.x = 0.0
-      tw.linear.y = 0.0
+    if vel_angle > math.pi/2 or vel_angle < -math.pi/2:
+      vel_r = 0.0
+    #convert polar to cartesian coordinate
+    tw.linear.x = math.cos(vel_angle)*vel_r
+    tw.linear.y = math.sin(vel_angle)*vel_r
     return tw
 
 def send_twist():
     global g_last_twist_send_time, g_target_twist, g_last_twist,\
-            g_vel_ramps, g_twist_pub,g_cogpos,g_vel_scales,g_vel_max,g_deadzone
-    g_target_twist=calc_vel(g_cogpos,g_vel_scales,g_vel_max,g_deadzone)
+            g_vel_ramps, g_twist_pub,g_cogpos,g_vel_scales,g_vel_max,g_deadzone,g_angular_max
+    g_target_twist=calc_vel(g_cogpos,g_vel_scales,g_vel_max,g_angular_max,g_deadzone)
     t_now = rospy.Time.now()
     g_last_twist = ramped_twist(g_last_twist, g_target_twist,
                               g_last_twist_send_time, t_now, g_vel_ramps,g_vel_max)
@@ -92,14 +92,13 @@ if __name__=="__main__":
     rospy.init_node('cogpos_twist')
     g_vel_scales[0] = fetch_param('~linear_scale_x', 1)
     g_vel_scales[1] = fetch_param('~linear_scale_y', 1)
-    g_vel_ramps[2] = fetch_param('~angular_accel', 1)
+    g_vel_scales[2] = fetch_param('~angular_scale_z',1)
     g_vel_ramps[0] = fetch_param('~linear_accel_x', 1)
     g_vel_ramps[1] = fetch_param('~linear_accel_y', 1)
-    g_vel_max[0] = fetch_param('~linear_maxspeed_x',0.1)
-    g_vel_max[1] = fetch_param('~linear_maxspeed_y',0.1)
-    g_vel_max[2] = fetch_param('~angular_maxspeed_z',0.1)
-    g_deadzone[0] = fetch_param('~deadzone_x',0.1)
-    g_deadzone[1] = fetch_param('~deadzone_y',0.1)
+    g_vel_ramps[2] = fetch_param('~angular_accel_z', 1)
+    g_vel_max = fetch_param('~linear_maxspeed',0.1)
+    g_angular_max = fetch_param("~angular_maxspeed",1.0)
+    g_deadzone = fetch_param('~deadzone',0.1)
     g_last_twist_send_time = rospy.Time.now()
     g_target_twist = Twist() # initializes to zero 
     g_last_twist = Twist()
